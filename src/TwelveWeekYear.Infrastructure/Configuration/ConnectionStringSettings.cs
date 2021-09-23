10925d30400fed0811132461577f522bd125ae80
Alex Watts <alexjwatts83@gmail.com>
2021-09-24 08:25:17 +1000

    wip

---
+++ src/TwelveWeekYear.Infrastructure/Configuration/ConnectionStringSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TwelveWeekYear.Infrastructure.Configuration
+{
+	public class ConnectionStringSettings
+	{
+		public const string Section = "ConnectionStrings";
+		public string Database { get; set; }
+		public string Redis { get; set; }
+	}
+}
