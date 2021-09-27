774a09c3f40e5f67bc3abe3e84026cfb9ab4cfb8
Alex Watts <alexjwatts83@gmail.com>
2021-09-27 17:03:26 +1000

    small code changes

--- src/TwelveWeekYear.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TwelveWeekYear.Application
-{
-    public class Class1
-    {
-    }
-}
--- src/TwelveWeekYear.Infrastructure/Persistence/DependencyInjection.cs
+++ src/TwelveWeekYear.Infrastructure/Persistence/DependencyInjection.cs
@@ -2,7 +2,7 @@
 
 namespace TwelveWeekYear.Infrastructure
 {
-    public class Class1
+    public class DependencyInjection
     {
     }
 }
