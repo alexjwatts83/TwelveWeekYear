f14e443be1716d6559f3e4d98131b49e07753bed
Alex Watts <alexjwatts83@gmail.com>
2021-09-28 08:49:19 +1000

    Added JsonObjectTypeHandler

---
+++ src/TwelveWeekYear.Infrastructure/Persistence/TypeHandler/JsonObjectTypeHandler.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Text.Json;
+
+namespace TwelveWeekYear.Infrastructure.Persistence.TypeHandler
+{
+	public class JsonObjectTypeHandler : SqlMapper.ITypeHandler
+	{
+		public void SetValue(IDbDataParameter parameter, object value)
+		{
+			parameter.Value = (value == null)
+				? (object)DBNull.Value
+				: JsonSerializer.Serialize(value);
+			parameter.DbType = DbType.String;
+		}
+
+		public object Parse(Type destinationType, object value)
+		{
+			return JsonSerializer.Deserialize(value.ToString(), destinationType);
+		}
+	}
+}
