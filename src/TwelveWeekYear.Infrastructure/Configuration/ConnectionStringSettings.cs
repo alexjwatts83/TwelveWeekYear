0c739158ca22827476c28ca4f9af48fdabbbc94d
Alex Watts <alexjwatts83@gmail.com>
2021-09-28 08:51:25 +1000

    implemented DependencyInjection

--- src/TwelveWeekYear.Infrastructure/Configuration/ConnectionStringSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TwelveWeekYear.Infrastructure.Configuration
-{
-	public class ConnectionStringSettings
-	{
-		public const string Section = "ConnectionStrings";
-		public string Database { get; set; }
-		public string Redis { get; set; }
-	}
-}
--- src/TwelveWeekYear.Infrastructure/Persistence/DependencyInjection.cs
+++ src/TwelveWeekYear.Infrastructure/Persistence/DependencyInjection.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TwelveWeekYear.Application.Interfaces;
+using TwelveWeekYear.Infrastructure.Persistence.Configuration;
+using TwelveWeekYear.Infrastructure.Persistence.Repositories;
 
 namespace TwelveWeekYear.Infrastructure
 {
-    public class DependencyInjection
-    {
-    }
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+		{
+			services.Configure<ConnectionStringSettings>(config.GetSection(ConnectionStringSettings.Section));
+
+			services.AddScoped(typeof(IBaseRepository), typeof(BaseRepository));
+
+			//SqlMapper.AddTypeHandler(typeof(IEnumerable<TodoItem>), new JsonObjectTypeHandler());
+
+			return services;
+		}
+	}
 }
