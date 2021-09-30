4d7bf00e4998ee807226a339f6e8b70d04dca79d
Alex Watts <alexjwatts83@gmail.com>
2021-10-01 08:27:03 +1000

    added some classes

--- src/TwelveWeekYear.Domain/Models/Goal.cs
+++ src/TwelveWeekYear.Domain/Models/Goal.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TwelveWeekYear.Domain.Models
 {
-	public class TweleveWeekYear
+	public class Goal
 	{
-		public DateTime Date { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
-		public IEnumerable<Goal> Goals { get; set; }
 	}
 }
