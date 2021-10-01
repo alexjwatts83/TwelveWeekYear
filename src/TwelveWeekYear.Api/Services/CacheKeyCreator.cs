fec3a6a39da1909bf9797277e5334db519c554fb
Alex Watts <alexjwatts83@gmail.com>
2021-10-02 08:26:07 +1000

    Add cache key creator service

---
+++ src/TwelveWeekYear.Api/Services/CacheKeyCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using TwelveWeekYear.Application.Interfaces;
+
+namespace TwelveWeekYear.Api.Services
+{
+	public class CacheKeyCreator : ICacheKeyCreator
+	{
+		public string GetCacheKeyNameFromObject(string keyPrefix, object parameters = null)
+		{
+			if (parameters == null)
+			{
+				return keyPrefix;
+			}
+			// TODO: figure out this works with a datetime type
+			var properties = parameters.GetType()
+									   .GetProperties()
+									   .Select(p => p.GetValue(parameters, null));
+
+			return keyPrefix + string.Join("_", properties);
+		}
+	}
+}
