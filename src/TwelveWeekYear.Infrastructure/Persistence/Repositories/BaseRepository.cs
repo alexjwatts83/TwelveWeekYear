c5bfb8b096dafe6c8a7ac58c2278076b3d95cefc
Alex Watts <alexjwatts83@gmail.com>
2022-01-04 20:23:55 +1100

    small change

--- src/TwelveWeekYear.Infrastructure/Persistence/Repositories/BaseRepository.cs
+++ src/TwelveWeekYear.Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using TwelveWeekYear.Application.Extensions;
 using TwelveWeekYear.Application.Interfaces;
