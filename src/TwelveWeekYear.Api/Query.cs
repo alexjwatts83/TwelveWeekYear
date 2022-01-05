d0a5f775ad82cd99a106a150bf35ae441b8e9032
Alex Watts <alexjwatts83@gmail.com>
2022-01-05 20:22:28 +1100

    Added Query class

---
+++ src/TwelveWeekYear.Api/Query.cs
@@ -0,0 +1,17 @@
+﻿using TwelveWeekYear.Domain.Models;
+
+namespace TwelveWeekYear.Api
+{
+	public class Query
+	{
+		public Goal GetGoal()
+		{
+			return new Goal()
+			{
+				Description = "New Goal",
+				Name = "Goal 1",
+				Id = 1
+			};
+		}
+	}
+}
