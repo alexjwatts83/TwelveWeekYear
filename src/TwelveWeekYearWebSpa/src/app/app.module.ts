2aa064782fe85c404a7ac478c4910f857d6be7ca
Alex Watts <alexjwatts83@gmail.com>
2022-01-09 10:35:57 +1100

    moved goals grid component

--- src/TwelveWeekYearWebSpa/src/app/app.module.ts
+++ src/TwelveWeekYearWebSpa/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { GoalsGridComponent } from './goals-grid/goals-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    GoalsGridComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- src/TwelveWeekYearWebSpa/src/app/goals/goals.module.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GoalsGridComponent } from './goals-grid/goals-grid.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GoalsGridComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    GoalsGridComponent
   ]
 })
 export class GoalsModule { }
