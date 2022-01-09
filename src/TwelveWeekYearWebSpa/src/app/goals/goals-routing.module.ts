0d472e3d844cf0fd01b96ee5d88034d8df60039f
Alex Watts <alexjwatts83@gmail.com>
2022-01-10 07:00:03 +1100

    Added goals grid component

--- src/TwelveWeekYearWebSpa/src/app/goals/goals-routing.module.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GoalsGridComponent } from './goals-grid/goals-grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'goals-grid', component: GoalsGridComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/TwelveWeekYearWebSpa/src/app/goals/goals.module.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GoalsRoutingModule } from './goals-routing.module';
+import { GoalsGridComponent } from './goals-grid/goals-grid.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GoalsGridComponent
+  ],
   imports: [
     CommonModule,
     GoalsRoutingModule
