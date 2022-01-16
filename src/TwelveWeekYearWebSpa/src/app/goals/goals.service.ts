d37187dd9596e62865d650c8ee78ed9969645b5b
Alex Watts <alexjwatts83@gmail.com>
2022-01-16 21:01:49 +1100

    wip goals service

--- src/TwelveWeekYearWebSpa/src/app/goals/goals.service.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Goal } from './models/goal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoalsService {
+  private _goals$ = new BehaviorSubject<Goal[]>([]);
 
   constructor() { }
+
+  getGoals(): Observable<Goal[]> {
+    return this._goals$.asObservable();
+  }
+
+  setGoals(profile: Goal[]) {
+      this._goals$.next(profile);
+  }
 }
