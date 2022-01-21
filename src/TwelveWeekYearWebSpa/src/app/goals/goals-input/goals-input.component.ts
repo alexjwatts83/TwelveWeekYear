63a4c894858ffd00590ac876c768899b232c4cab
Alex Watts <alexjwatts83@gmail.com>
2022-01-21 20:47:58 +1100

    wip adding goal

--- src/TwelveWeekYearWebSpa/src/app/goals/goals-input/goals-input.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals-input/goals-input.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { GoalsService } from '../goals.service';
+import { Goal } from '../models/goal';
+import {v4 as uuidv4} from 'uuid';
 
 @Component({
   selector: 'app-goals-input',
@@ -17,7 +19,13 @@ export class GoalsInputComponent implements OnInit {
     });
   }
 
-  onSubmit(f: any) {
-    console.log({f});
+  onSubmit(f: FormGroupDirective) {
+    let myuuid = uuidv4();
+    const goal: Goal = {
+      description: f.value.description,
+      id: myuuid
+    };
+    console.log({f, value: f.value, goal});
+    this.service.addGoal(goal);
   }
 }
