672bdd1f367bd49d49f96fef5283fda86bb42e1e
Alex Watts <alexjwatts83@gmail.com>
2022-01-27 21:32:46 +1100

    open accordion by default

--- src/TwelveWeekYearWebSpa/src/app/goals/goals-group/goals-group.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals-group/goals-group.component.ts
@@ -11,14 +11,22 @@ import { Goal, GoalTypes } from '../models/goal';
 export class GoalsGroupComponent implements OnInit {
   @Input() title!: string;
   @Input() goalType: GoalTypes = GoalTypes.None;
+  @Input() openAccordion: boolean = false;
 
   panelOpenState = false;
   data$!: Observable<Goal[]>;
-  constructor(private service: GoalsService) { }
+  constructor(private service: GoalsService) { 
+    if (this.openAccordion) {
+      this.panelOpenState = true;
+    }
+  }
 
   ngOnInit(): void {
     this.data$ = this.service.getGoals(this.goalType);
-
+    if (this.openAccordion) {
+      this.panelOpenState = true;
+    }
+    console.log({panelOpenState: this.panelOpenState});
     // this.data$.subscribe((x) => {
     //   // console.log({x, goalType: this.goalType, title: this.title});
     //   // this.goals = x;
