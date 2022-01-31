54b1d6ebfa94e05d269fe9e64ec4491b276a8a11
Alex Watts <alexjwatts83@gmail.com>
2022-02-01 07:11:51 +1100

    wip goals to treelist

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year-goals-treelist/twelve-week-year-goals-treelist.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year-goals-treelist/twelve-week-year-goals-treelist.component.ts
@@ -10,33 +10,34 @@ import { Goal } from 'src/app/goals/models';
  * Food data with nested structure.
  * Each node has a name and an optional list of children.
  */
-interface FoodNode {
+interface GoalNode {
+  data: any;
   name: string;
-  children?: FoodNode[];
+  children?: GoalNode[];
 }
 
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }],
-  },
-  {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
-      },
-      {
-        name: 'Orange',
-        children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
-      },
-    ],
-  },
-];
+// const TREE_DATA: GoalNode[] = [
+//   {
+//     name: 'Fruit',
+//     children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }],
+//   },
+//   {
+//     name: 'Vegetables',
+//     children: [
+//       {
+//         name: 'Green',
+//         children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
+//       },
+//       {
+//         name: 'Orange',
+//         children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
+//       },
+//     ],
+//   },
+// ];
 
 /** Flat node with expandable and level information */
-interface ExampleFlatNode {
+interface ExpandableFlatNode {
   expandable: boolean;
   name: string;
   level: number;
@@ -49,8 +50,8 @@ interface ExampleFlatNode {
 })
 export class TwelveWeekYearGoalsTreelistComponent implements OnInit {
   @Input() goals: Goal[] = [];
-  
-  private _transformer = (node: FoodNode, level: number) => {
+
+  private _transformer = (node: GoalNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
       name: node.name,
@@ -58,7 +59,7 @@ export class TwelveWeekYearGoalsTreelistComponent implements OnInit {
     };
   };
 
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
+  treeControl = new FlatTreeControl<ExpandableFlatNode>(
     (node) => node.level,
     (node) => node.expandable
   );
@@ -73,10 +74,37 @@ export class TwelveWeekYearGoalsTreelistComponent implements OnInit {
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   constructor() {
-    this.dataSource.data = TREE_DATA;
+    // let data = this.goals.map(x => {
+    //   let goalNode: GoalNode = {
+    //     goal: x,
+    //     name: x.description
+    //   }
+    //   return goalNode;
+    // })
+    // this.dataSource.data = data;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let data = this.goals.map(x => {
+      let goalNode: GoalNode = {
+        data: x,
+        name: x.description
+      }
+      let children = x.tasks.map(t => {
+        let child: GoalNode = {
+          data: t,
+          name: t.description
+        }
+        return child;
+      });
 
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+      goalNode.children = children;
+      
+      return goalNode;
+    })
+    this.dataSource.data = data;
+    console.log({goals: this.goals, data});
+  }
+
+  hasChild = (_: number, node: ExpandableFlatNode) => node.expandable;
 }
