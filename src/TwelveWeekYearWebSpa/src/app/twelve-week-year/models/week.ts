b42e46900c473bbb0eefa7697d40d88535c63903
Alex Watts <alexjwatts83@gmail.com>
2022-02-05 08:57:04 +1100

    changed number type to number

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/models/week.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/models/week.ts
@@ -2,7 +2,7 @@ import { WeekDay } from "./week-day";
 import { TaskComments } from "./task-comments";
 
 export interface Week {
-  number: Number;
+  number: number;
   date: Date;
   days: WeekDay[];
   taskComments: TaskComments[];
