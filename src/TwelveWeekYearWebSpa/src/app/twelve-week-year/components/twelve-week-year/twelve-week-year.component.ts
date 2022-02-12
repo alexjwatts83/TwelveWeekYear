5564e6b92a8e674ea56b82d9ab2b42028c5fdfca
Alex Watts <alexjwatts83@gmail.com>
2022-02-13 07:53:57 +1100

    refactoring

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year/twelve-week-year.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year/twelve-week-year.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { GoalsService } from 'src/app/goals/goals.service';
-import { Goal, GoalTypes } from 'src/app/goals/models';
-import { Week, WeekDayResult } from '../../models';
-import { TwelveWeekYearData, TwelveWeekYearService } from '../../twelve-week-year.service';
+import { Observable } from 'rxjs';
+import { WeekDayResult } from '../../models';
+import {
+  TwelveWeekYearData,
+  TwelveWeekYearService,
+} from '../../twelve-week-year.service';
 
 @Component({
   selector: 'app-twelve-week-year',
@@ -11,28 +12,18 @@ import { TwelveWeekYearData, TwelveWeekYearService } from '../../twelve-week-yea
   styleUrls: ['./twelve-week-year.component.scss'],
 })
 export class TwelveWeekYearComponent implements OnInit {
-  // weeks: Week[] = [];
-  // private taskResults: WeekDayResult[] = [];
-  // taskResults$!: Observable<WeekDayResult[]>;
   data$!: Observable<TwelveWeekYearData>;
   private _data: TwelveWeekYearData | undefined;
-  // twelveWeekYearGoals: Goal[] = [];
 
-  // private weekOneFirstGoalId = '';
-
-  constructor(
-    // private service: GoalsService,
-    private service: TwelveWeekYearService) {
+  constructor(private service: TwelveWeekYearService) {
     this.data$ = this.service.getTwelveWeekData();
   }
 
   ngOnInit(): void {
     if (this.data$) {
       this.data$.subscribe((x) => {
-        console.log({x});
+        console.log({ x });
         this._data = x;
-        // this.twelveWeekYearGoals = x;
-        // this.taskResults$ = this.init();
       });
     }
   }
@@ -42,7 +33,6 @@ export class TwelveWeekYearComponent implements OnInit {
     goalId: string,
     taskId: string
   ): number {
-
     const groupBy = (array: any[], key: string) => {
       // Return the end result
       return array.reduce((result, currentValue) => {
@@ -59,46 +49,18 @@ export class TwelveWeekYearComponent implements OnInit {
       (x) =>
         x.goalId == goalId && x.weekNumber == weekNumber && taskId === x.taskId
     );
-    // if (weekNumber === 1 && this.weekOneFirstGoalId === goalId) {
-    //   // Accepts the array and key
-
-    //   const taskedGrouped = groupBy(thisTaskResults, 'date');
-
-    //   // console.log({});
-    //   if (thisTaskResults.length === 0) {
-    //     console.log({
-    //       weekNumber,
-    //       goalId,
-    //       taskedGrouped,
-    //       thisTaskResults,
-    //       all: this.taskResults,
-    //     });
-    //   } else {
-    //     console.log({
-    //       weekNumber,
-    //       goalId,
-    //       taskedGrouped,
-    //       thisTaskResults,
-    //       all: this.taskResults,
-    //     });
-    //   }
-    // }
 
     if (thisTaskResults == null) {
       return 0;
     }
 
-    if (thisTaskResults.some(t => t.subTaskId != null)) {
+    if (thisTaskResults.some((t) => t.subTaskId != null)) {
       // TODO: refactor this
       const taskedGrouped = groupBy(thisTaskResults, 'date');
-      console.log({goal: 'has subtasks', weekNumber,
-      goalId,thisTaskResults, taskedGrouped});
-
       let count = 0;
-      let k: keyof typeof taskedGrouped;  // Type is "one" | "two" | "three"
+      let k: keyof typeof taskedGrouped;
       for (k in taskedGrouped) {
-        const v = taskedGrouped[k];  // OK
-        console.log({k, v});
+        const v = taskedGrouped[k];
         if (v.every((c: WeekDayResult) => c.completed)) {
           count++;
         }
@@ -106,20 +68,6 @@ export class TwelveWeekYearComponent implements OnInit {
       return count;
     }
 
-    return thisTaskResults.filter(x => x.completed).length;
+    return thisTaskResults.filter((x) => x.completed).length;
   }
-
-  // private groupBy<T>(list: T[], keyGetter: any) {
-  //   const map = new Map();
-  //   list.forEach((item) => {
-  //     const key = keyGetter(item);
-  //     const collection = map.get(key);
-  //     if (!collection) {
-  //       map.set(key, [item]);
-  //     } else {
-  //       collection.push(item);
-  //     }
-  //   });
-  //   return map;
-  // }
 }
