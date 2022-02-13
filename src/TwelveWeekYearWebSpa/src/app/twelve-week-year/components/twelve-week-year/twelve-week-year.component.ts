e2db81cccfa4aeebd6a6b3c08068b959e27700c9
Alex Watts <alexjwatts83@gmail.com>
2022-02-14 07:34:56 +1100

    moved the groupBy to a function

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year/twelve-week-year.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year/twelve-week-year.component.ts
@@ -28,23 +28,23 @@ export class TwelveWeekYearComponent implements OnInit {
     }
   }
 
+  private groupBy(array: any[], key: string) {
+    // Return the end result
+    return array.reduce((result, currentValue) => {
+      // If an array already present for key, push it to the array. Else create an array and push the object
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+      return result;
+    }, {}); // empty object is the initial value for result object
+  }
+
   getGoalDailyProgressCount(
     weekNumber: number,
     goalId: string,
     taskId: string
   ): number {
-    const groupBy = (array: any[], key: string) => {
-      // Return the end result
-      return array.reduce((result, currentValue) => {
-        // If an array already present for key, push it to the array. Else create an array and push the object
-        (result[currentValue[key]] = result[currentValue[key]] || []).push(
-          currentValue
-        );
-        // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
-        return result;
-      }, {}); // empty object is the initial value for result object
-    };
-
     let thisTaskResults = this._data?.taskResults.filter(
       (x) =>
         x.goalId == goalId && x.weekNumber == weekNumber && taskId === x.taskId
@@ -56,7 +56,7 @@ export class TwelveWeekYearComponent implements OnInit {
 
     if (thisTaskResults.some((t) => t.subTaskId != null)) {
       // TODO: refactor this
-      const taskedGrouped = groupBy(thisTaskResults, 'date');
+      const taskedGrouped = this.groupBy(thisTaskResults, 'date');
       let count = 0;
       let k: keyof typeof taskedGrouped;
       for (k in taskedGrouped) {
