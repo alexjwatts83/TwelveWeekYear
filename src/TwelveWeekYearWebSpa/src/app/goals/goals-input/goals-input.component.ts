1a6fbb044fbd7254526e519966500079ab9f6e9d
Alex Watts <alexjwatts83@gmail.com>
2022-02-16 07:11:30 +1100

    wip setup for adding tasks

--- src/TwelveWeekYearWebSpa/src/app/goals/goals-input/goals-input.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals-input/goals-input.component.ts
@@ -1,23 +1,35 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
 import { GoalsService } from '../goals.service';
 import { GoalTypes } from '../models';
 
 @Component({
   selector: 'app-goals-input',
   templateUrl: './goals-input.component.html',
-  styleUrls: ['./goals-input.component.scss']
+  styleUrls: ['./goals-input.component.scss'],
 })
 export class GoalsInputComponent implements OnInit {
   @Input() goalType!: GoalTypes;
 
   goalInputForm!: FormGroup;
-  
-  constructor(private service: GoalsService) { }
+
+  get canAddTasks(): boolean {
+    if (this.goalType) {
+      return this.goalType === GoalTypes.TwelveWeekYear;
+    }
+    return false;
+  }
+
+  constructor(private service: GoalsService) {}
 
   ngOnInit(): void {
     this.goalInputForm = new FormGroup({
-      description: new FormControl('', [ Validators.required ]),
+      description: new FormControl('', [Validators.required]),
     });
   }
 
