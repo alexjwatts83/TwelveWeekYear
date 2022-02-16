4ec832cdd2153e82d6554d89800e5bf13b2a0499
Alex Watts <alexjwatts83@gmail.com>
2022-02-17 07:44:37 +1100

    wip task input with subtasks using form array

--- src/TwelveWeekYearWebSpa/src/app/goals/task-input/task-input.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/task-input/task-input.component.ts
@@ -1,25 +1,37 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
 import { Task } from '../models';
 import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-task-input',
   templateUrl: './task-input.component.html',
-  styleUrls: ['./task-input.component.scss']
+  styleUrls: ['./task-input.component.scss'],
 })
 export class TaskInputComponent implements OnInit {
   taskInputForm!: FormGroup;
 
   @Output() taskAdded = new EventEmitter<Task>();
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.taskInputForm = new FormGroup({
-      id: new FormControl('', [Validators.required]),
+    this.taskInputForm = this.fb.group({
+      id: new FormControl(uuidv4(), [Validators.required]),
       description: new FormControl('', [Validators.required]),
+      subTasks: this.fb.array([]),
     });
+  }
+
+  get subTasks() {
+    return this.taskInputForm.controls['lessons'] as FormArray;
   }
 
   onSubmit(f: FormGroupDirective) {
@@ -30,4 +42,12 @@ export class TaskInputComponent implements OnInit {
     this.taskAdded.emit(task);
   }
 
+  addSubTask() {
+    const lessonForm = this.fb.group({
+      id: [uuidv4(), Validators.required],
+      description: ['Keep it real', Validators.required]
+    });
+  
+    this.subTasks.push(lessonForm);
+  }
 }
