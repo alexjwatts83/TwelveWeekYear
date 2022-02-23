af613aa1a7f3bd709584d3bad529e17082f98754
Alex Watts <alexjwatts83@gmail.com>
2022-02-24 07:54:11 +1100

    refactored group using a type safe version

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year/twelve-week-year.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/twelve-week-year/twelve-week-year.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TwelveWeekYear, WeekDayResult } from '../../models';
-import {
-  TwelveWeekYearService,
-} from '../../twelve-week-year.service';
+import { TwelveWeekYearService } from '../../twelve-week-year.service';
 
 @Component({
   selector: 'app-twelve-week-year',
@@ -27,17 +25,31 @@ export class TwelveWeekYearComponent implements OnInit {
     }
   }
 
-  private groupBy(array: any[], key: string) {
-    // Return the end result
-    return array.reduce((result, currentValue) => {
-      // If an array already present for key, push it to the array. Else create an array and push the object
-      (result[currentValue[key]] = result[currentValue[key]] || []).push(
-        currentValue
-      );
-      // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
-      return result;
-    }, {}); // empty object is the initial value for result object
+  private groupBy<Val, Key>(list: Val[], predicate: (value: Val) => Key): Map<Key, Val[]> {
+    const map = new Map();
+    list.forEach((item) => {
+      const key = predicate(item);
+      const collection = map.get(key);
+      if (!collection) {
+        map.set(key, [item]);
+      } else {
+        collection.push(item);
+      }
+    });
+    return map;
   }
+
+  // private groupBy(array: any[], key: string) {
+  //   // Return the end result
+  //   return array.reduce((result, currentValue) => {
+  //     // If an array already present for key, push it to the array. Else create an array and push the object
+  //     (result[currentValue[key]] = result[currentValue[key]] || []).push(
+  //       currentValue
+  //     );
+  //     // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+  //     return result;
+  //   }, {}); // empty object is the initial value for result object
+  // }
 
   getGoalDailyProgressCount(
     weekNumber: number,
@@ -55,15 +67,23 @@ export class TwelveWeekYearComponent implements OnInit {
 
     if (thisTaskResults.some((t) => t.subTaskId != null)) {
       // TODO: refactor this
-      const taskedGrouped = this.groupBy(thisTaskResults, 'date');
+      const taskedGrouped = this.groupBy<WeekDayResult, Date>(
+        thisTaskResults,
+        (t) => t.date
+      );
       let count = 0;
-      let k: keyof typeof taskedGrouped;
-      for (k in taskedGrouped) {
-        const v = taskedGrouped[k];
-        if (v.every((c: WeekDayResult) => c.completed)) {
-          count++;
-        }
+
+      for(let [key, value] of taskedGrouped) {
+        let allCompleted = value.every((result: WeekDayResult) => result.completed);
+        count = allCompleted ? count + 1 : count;
       }
+      // let k: keyof typeof taskedGrouped;
+      // for (k in taskedGrouped) {
+      //   const v = taskedGrouped[k];
+      //   if (v.every((c: WeekDayResult) => c.completed)) {
+      //     count++;
+      //   }
+      // }
       return count;
     }
 
