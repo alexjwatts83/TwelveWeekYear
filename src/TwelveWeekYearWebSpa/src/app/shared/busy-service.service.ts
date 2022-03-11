b60594efb52295d7f3ed227c5b9d2572bcd2b43e
Alex Watts <alexjwatts83@gmail.com>
2022-03-12 08:04:56 +1100

    code clean up

--- src/TwelveWeekYearWebSpa/src/app/shared/busy-service.service.ts
+++ src/TwelveWeekYearWebSpa/src/app/shared/busy-service.service.ts
@@ -11,27 +11,12 @@ export class BusyService {
 
   busy(){
     this.busyRequestCount++;
-    console.log({busy: this.busyRequestCount});
-    // this.spinnerService.show(undefined, {
-    //   type: 'line-scale-party',
-    //   bdColor: 'rgba(255,255,255,0.5)',
-    //   color: '#333333',
-    //   fullScreen: true
-    // });
     this.spinnerService.show();
-  
-    // this.spinnerService.spinnerObservable.subscribe(c=>{
-    //   c.show.
-    // });
-    // setTimeout(() => {
-    //   console.log('finished');
-    // }, 3000);
   }
 
   idle(){
     setTimeout(() => {
       this.busyRequestCount--;
-      console.log({idle: this.busyRequestCount});
       if(this.busyRequestCount <=0) {
         this.busyRequestCount = 0;
         this.spinnerService.hide();
