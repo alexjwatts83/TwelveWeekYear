a7f00ad6315ab3ab0ceee1d54e6d6a6e7aa55227
Alex Watts <alexjwatts83@gmail.com>
2022-03-12 08:06:11 +1100

    removed more console logs

--- src/TwelveWeekYearWebSpa/src/app/app.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/app.component.ts
@@ -41,13 +41,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     /** spinner starts on init */
-    console.log({ngOnInit: true});
     this.busyService.busy();
 
     setTimeout(() => {
       /** spinner ends after 5 seconds */
       this.busyService.idle();
-      console.log({hide: true});
-    }, 1000);
+    }, 300);
   }
 }
