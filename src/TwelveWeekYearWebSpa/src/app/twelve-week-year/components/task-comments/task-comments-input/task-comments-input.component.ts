8d6ba0f3d64fb5c51a53500b6de2e64cf706fc0b
Alex Watts <alexjwatts83@gmail.com>
2022-03-13 08:43:07 +1100

    code prettying

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/task-comments/task-comments-input/task-comments-input.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/task-comments/task-comments-input/task-comments-input.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
 import { Week } from 'src/app/twelve-week-year/models';
 import { Task } from 'src/app/goals/models';
 import { TaskCommentsService } from '../task-comments.service';
@@ -7,25 +13,29 @@ import { TaskCommentsService } from '../task-comments.service';
 @Component({
   selector: 'app-task-comments-input',
   templateUrl: './task-comments-input.component.html',
-  styleUrls: ['./task-comments-input.component.scss']
+  styleUrls: ['./task-comments-input.component.scss'],
 })
 export class CommentsInputComponent implements OnInit {
   @Input() task!: Task;
   @Input() week!: Week;
-  
+
   commentsForm!: FormGroup;
-  constructor(private service: TaskCommentsService, private fb: FormBuilder) { }
+  constructor(private service: TaskCommentsService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.commentsForm = new FormGroup({
-      comment: new FormControl('', [ Validators.required ]),
+      comment: new FormControl('', [Validators.required]),
     });
   }
 
   onSubmit(f: FormGroupDirective) {
-    console.log({onSubmit: true, f});
-    this.service.addComment(this.task.id, this.week.number, f.value.comment, new Date());
-    // this.service.addGoal(f.value.description, this.goalType);
+    console.log({ onSubmit: true, f });
+    this.service.addComment(
+      this.task.id,
+      this.week.number,
+      f.value.comment,
+      new Date()
+    );
     this.commentsForm.reset();
     f.resetForm();
   }
