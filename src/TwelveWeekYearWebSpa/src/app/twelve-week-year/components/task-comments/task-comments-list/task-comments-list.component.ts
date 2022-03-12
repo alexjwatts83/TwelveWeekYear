2b136a78dd8c4950e20f791eaf6dbb2927bd1432
Alex Watts <alexjwatts83@gmail.com>
2022-03-13 08:44:20 +1100

    removed console logs

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/task-comments/task-comments-list/task-comments-list.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/task-comments/task-comments-list/task-comments-list.component.ts
@@ -19,12 +19,9 @@ export class CommentsListComponent implements OnInit {
   constructor(private service: TaskCommentsService) {}
 
   ngOnInit(): void {
-    // console.log({taskId: this.task, weekNumber: this.week});
     this.data$ = this.service.getComments(this.task.id, this.week.number);
     this.data$.subscribe((taskComments) => {
-      // console.log({taskComments});
       taskComments.forEach((tc) => {
-        // console.log({tc});
         this.comments = tc.comments;
       });
     });
