954da8555b7b796b2224b0785f88fbf5a7df10e5
Alex Watts <alexjwatts83@gmail.com>
2022-03-16 07:33:21 +1100

    removed more logs

--- src/TwelveWeekYearWebSpa/src/app/goals/goals-input-service.service.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/goals-input-service.service.ts
@@ -3,19 +3,17 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Task } from './models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GoalsInputServiceService {
   private data: Task[] = [];
   private _tasks$ = new BehaviorSubject<Task[]>(this.data);
   private _resetTrigger$ = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  constructor() {}
 
   getTasks(): Observable<Task[]> {
-    // console.log({getTasks: true});
-    return this._tasks$
-      .asObservable();
+    return this._tasks$.asObservable();
   }
 
   resetTriggered(): Observable<boolean> {
@@ -23,22 +21,18 @@ export class GoalsInputServiceService {
   }
 
   resetTasks() {
-    // console.log({getTasks: true});
     this.data = [];
     this.setTasks();
     this._resetTrigger$.next(true);
   }
 
   private setTasks() {
-    // console.log({setTasks: true});
     this._tasks$.next(this.data);
   }
 
   addTask(task: Task): void {
-    // console.log({addTask: task});
-    this.data.push({...task});
+    this.data.push({ ...task });
 
     this.setTasks();
-    // console.log({data: this.data});
   }
 }
