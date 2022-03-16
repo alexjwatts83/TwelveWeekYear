abccf4fd6805ad81fddcd12c1c6e33cb7d5f5187
Alex Watts <alexjwatts83@gmail.com>
2022-03-17 07:25:08 +1100

    wip mock data service

--- src/TwelveWeekYearWebSpa/src/app/goals/models/goal.ts
+++ src/TwelveWeekYearWebSpa/src/app/goals/models/goal.ts
@@ -1,8 +1,8 @@
 export enum GoalTypes {
-  None,
-  ThreeToFiveYear,
-  ThisYear,
-  TwelveWeekYear
+  None = 0,
+  ThreeToFiveYear = 1,
+  ThisYear = 2,
+  TwelveWeekYear = 3
 }
 export interface SubTask {
   id: string;
