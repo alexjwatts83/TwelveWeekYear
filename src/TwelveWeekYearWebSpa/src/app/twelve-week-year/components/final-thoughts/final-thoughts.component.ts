85a5a0907d99868d4071bea7a356093101e07643
Alex Watts <alexjwatts83@gmail.com>
2022-03-27 17:19:30 +1100

    formatting

--- src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/final-thoughts/final-thoughts.component.ts
+++ src/TwelveWeekYearWebSpa/src/app/twelve-week-year/components/final-thoughts/final-thoughts.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
-import { TaskCommentsService } from '../task-comments/task-comments.service';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormGroupDirective,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-final-thoughts',
   templateUrl: './final-thoughts.component.html',
-  styleUrls: ['./final-thoughts.component.scss']
+  styleUrls: ['./final-thoughts.component.scss'],
 })
 export class FinalThoughtsComponent implements OnInit {
-
   commentsForm!: FormGroup;
-  constructor(private service: TaskCommentsService) {
+  constructor() {
     this.commentsForm = new FormGroup({
       comment: new FormControl('', [Validators.required]),
     });
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onSubmit(f: FormGroupDirective) {
     this.commentsForm.reset();
