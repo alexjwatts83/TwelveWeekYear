40a62211ca14c1ad065ea4ed4727d79630dcab82
Alex Watts <alexjwatts83@gmail.com>
2022-04-12 07:11:53 +1000

    Removed name and added tasks

--- src/TwelveWeekYear.Domain/Models/Subtask.cs
+++ src/TwelveWeekYear.Domain/Models/Subtask.cs
@@ -3,7 +3,6 @@
 	public class Subtask
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
 		public string Description { get; set; }
 	}
 }
---
+++ src/TwelveWeekYear.Domain/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TwelveWeekYear.Domain.Models
+{
+	public class Task
+	{
+		public int Id { get; set; }
+		public string Description { get; set; }
+		public List<Subtask> Subtasks { get; set; }
+	}
+}
