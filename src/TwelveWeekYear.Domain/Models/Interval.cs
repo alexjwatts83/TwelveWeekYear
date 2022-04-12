a5b33e2f9e4654bf470d486bb4ec17405b059efb
Alex Watts <alexjwatts83@gmail.com>
2022-04-13 07:04:07 +1000

    Added interval class

---
+++ src/TwelveWeekYear.Domain/Models/Interval.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TwelveWeekYear.Domain.Models
+{
+	public class Interval
+	{
+		public int Id { get; set; }
+		public DateTime DateStart { get; set; }
+		public DateTime DateEnd { get; set; }
+	}
+}
