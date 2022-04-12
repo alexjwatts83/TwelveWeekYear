0c68b9f6de2838322fff1b484ce015ac2f7ea4ca
Alex Watts <alexjwatts83@gmail.com>
2022-04-13 07:02:53 +1000

    Added dayresults class

---
+++ src/TwelveWeekYear.Domain/Models/DaysResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwelveWeekYear.Domain.Models
+{
+	public class DaysResult
+	{
+		public int Id { get; set; }
+		public DateTime Date { get; set; }
+		public Goal Goal { get; set; }
+		public string Notes { get; set; }
+		public bool Completed { get; set; }
+	}
+}
