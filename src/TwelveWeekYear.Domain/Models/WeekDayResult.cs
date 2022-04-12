5c701a20f1d6a48ee3c11dd296262e789f7d8897
Alex Watts <alexjwatts83@gmail.com>
2022-04-13 07:08:44 +1000

    Added Weekday Result

---
+++ src/TwelveWeekYear.Domain/Models/WeekDayResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwelveWeekYear.Domain.Models
+{
+	public class WeekDayResult
+	{
+		public int Id { get; set; }
+		public int WeekNumber { get; set; }
+		public DateTime Date { get; set; }
+		public string Name { get; set; }
+		public Goal Goal { get; set; }
+		public Task Task { get; set; }
+		public Subtask Subtask { get; set; }
+		public bool Completed { get; set; }
+	}
+}
