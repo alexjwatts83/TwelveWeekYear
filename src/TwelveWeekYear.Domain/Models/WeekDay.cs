6b456c7c71f68ec6dd5c503d68dd58a034bde5ab
Alex Watts <alexjwatts83@gmail.com>
2022-04-13 07:05:10 +1000

    Added weekday class

---
+++ src/TwelveWeekYear.Domain/Models/WeekDay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwelveWeekYear.Domain.Models
+{
+	public class WeekDay
+	{
+		public int Id { get; set; }
+		public DateTime Date { get; set; }
+		public string Description { get; set; }
+	}
+}
