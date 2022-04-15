440a62dc8c9d87eee79346991ff88df6d45a01a9
Alex Watts <alexjwatts83@gmail.com>
2022-04-15 23:20:23 +1000

    updated services

---
+++ src/TwelveWeekYear.Api/Services/TwelveWeekYearService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwelveWeekYear.Api.Services
+{
+	public class TwelveWeekYearService
+	{
+	}
+}
---
+++ src/TwelveWeekYear.WebApp/Services/TwelveWeekYearService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TwelveWeekYear.Application.Interfaces;
+using TwelveWeekYear.Domain.Models;
+
+namespace TwelveWeekYear.WebApp.Services
+{
+	public class TwelveWeekYearService: ITwelveWeekYearService
+	{
+
+	}
+}
