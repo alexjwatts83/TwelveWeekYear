7333ef9306e6a2e6805c0f84609a9852b858ecd5
Alex Watts <alexjwatts83@gmail.com>
2022-04-15 23:21:32 +1000

    wip TwelveWeekYearService

--- src/TwelveWeekYear.Api/Services/TwelveWeekYearService.cs
+++ src/TwelveWeekYear.Api/Services/TwelveWeekYearService.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TwelveWeekYear.Application.Interfaces;
+using TwelveWeekYear.Domain.Models;
 
 namespace TwelveWeekYear.Api.Services
 {
-	public class TwelveWeekYearService
+	public class TwelveWeekYearService : ITwelveWeekYearService
 	{
+		Task<TweleveWeekYear> ITwelveWeekYearService.GetData()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
