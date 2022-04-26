c8b3eeec52a01a1af70f04c417d2cb25ce04bd5b
Alex Watts <alexjwatts83@gmail.com>
2022-04-27 07:25:15 +1000

    WIP Adding annotations

--- src/TwelveWeekYear.Domain/Models/DaysResult.cs
+++ src/TwelveWeekYear.Domain/Models/DaysResult.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TwelveWeekYear.Domain.Models
 {
 	public class DaysResult
 	{
+		[Key]
 		public int Id { get; set; }
 		public DateTime Date { get; set; }
+		[Required]
+		public int GoalId { get; set; }
 		public Goal Goal { get; set; }
 		public string Notes { get; set; }
+		[Required]
 		public bool Completed { get; set; }
 	}
 }
