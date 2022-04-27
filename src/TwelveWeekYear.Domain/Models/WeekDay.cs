9433b261364f2e816974b059e2f9b7b9610e5b51
Alex Watts <alexjwatts83@gmail.com>
2022-04-28 06:55:14 +1000

    More annotations

--- src/TwelveWeekYear.Domain/Models/WeekDay.cs
+++ src/TwelveWeekYear.Domain/Models/WeekDay.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TwelveWeekYear.Domain.Models
 {
 	public class WeekDay
 	{
+		[Key]
 		public int Id { get; set; }
+		[Required]
 		public DateTime Date { get; set; }
+		[Required]
 		public string Description { get; set; }
 	}
 }
--- src/TwelveWeekYear.Domain/Models/WeekDayResult.cs
+++ src/TwelveWeekYear.Domain/Models/WeekDayResult.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TwelveWeekYear.Domain.Models
 {
 	public class WeekDayResult
 	{
+		[Key]
 		public int Id { get; set; }
+		[Required]
 		public int WeekNumber { get; set; }
+		[Required]
 		public DateTime Date { get; set; }
+		[Required]
 		public string Name { get; set; }
+		public int GoalId { get; set; }
 		public Goal Goal { get; set; }
+		public int TaskId { get; set; }
 		public Task Task { get; set; }
+		public int SubtaskId { get; set; }
 		public Subtask Subtask { get; set; }
+		[Required]
 		public bool Completed { get; set; }
 	}
 }
