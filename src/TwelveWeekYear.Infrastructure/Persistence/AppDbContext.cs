832e065009ade5359681cbfc83db6252f8384889
Alex Watts <alexjwatts83@gmail.com>
2022-04-29 07:17:02 +1000

    Adding more dbsets

--- src/TwelveWeekYear.Infrastructure/Persistence/AppDbContext.cs
+++ src/TwelveWeekYear.Infrastructure/Persistence/AppDbContext.cs
@@ -16,6 +16,15 @@ namespace TwelveWeekYear.Infrastructure.Persistence
 		}
 
 		public DbSet<DaysResult> DaysResults { get; set; }
+		public DbSet<Goal> Goals { get; set; }
+		public DbSet<GoalAction> GoalActions { get; set; }
+		public DbSet<Interval> Intervals { get; set; }
+		public DbSet<Subtask> Subtasks { get; set; }
+		public DbSet<Domain.Models.Task> Tasks { get; set; }
+		public DbSet<TweleveWeekYear> TweleveWeekYears { get; set; }
+		public DbSet<TweleveWeekYearWeek> TweleveWeekYearWeeks { get; set; }
+		public DbSet<WeekDay> WeekDays { get; set; }
+		public DbSet<WeekDayResult> WeekDayResults { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
