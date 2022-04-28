482b77d48b18c7b08c248a3ff78cc5413d12e244
Alex Watts <alexjwatts83@gmail.com>
2022-04-29 07:10:10 +1000

    wip db context

---
+++ src/TwelveWeekYear.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwelveWeekYear.Domain.Models;
+
+namespace TwelveWeekYear.Infrastructure.Persistence
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<DaysResult> DaysResults { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+		}
+	}
+}
