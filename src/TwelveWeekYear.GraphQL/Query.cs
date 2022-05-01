4fdc34aa382c7cfa86253e21e5d4f06432aca710
Alex Watts <alexjwatts83@gmail.com>
2022-05-01 13:09:33 +1000

    added a very simple query for Goal Types

--- src/TwelveWeekYear.GraphQL/Query.cs
+++ src/TwelveWeekYear.GraphQL/Query.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HotChocolate;
+using HotChocolate.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using TwelveWeekYear.Domain.Models;
+using TwelveWeekYear.Infrastructure.Persistence;
 
 namespace TwelveWeekYear.GraphQL
 {
+	[GraphQLDescription("Represents the queries available.")]
 	public class Query
 	{
+		[UseDbContext(typeof(AppDbContext))]
+		[UseFiltering]
+		[UseSorting]
+		[GraphQLDescription("Gets the queryable platform.")]
+		public IQueryable<GoalType> GetGoalTypes([ScopedService] AppDbContext context)
+		{
+			return context.GoalTypes;
+		}
 	}
 }
