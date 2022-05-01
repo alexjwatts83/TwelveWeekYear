c630bac64872c61c99bd74c563961294c3fd30ee
Alex Watts <alexjwatts83@gmail.com>
2022-05-01 13:27:17 +1000

    db seeding working but query is not working

--- src/TwelveWeekYear.GraphQL/Program.cs
+++ src/TwelveWeekYear.GraphQL/Program.cs
@@ -1,26 +1,58 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using TwelveWeekYear.Infrastructure.Persistence;
+using TwelveWeekYear.Infrastructure;
 
 namespace TwelveWeekYear.GraphQL
 {
-    public class Program
+	public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+			var host = CreateWebHostBuilder(args).Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+			using (var scope = host.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+
+				try
+				{
+					var context = services
+						.GetRequiredService<AppDbContext>();
+
+					context.Database.Migrate();
+
+					AppDbContextSeeder.Seed(context);
+				}
+				catch (Exception ex)
+				{
+					var logger = services
+						.GetRequiredService<ILogger<Program>>();
+
+					logger.LogError(ex, "An error occurred while " +
+						"migrating or initializing the database.");
+
+					throw;
+				}
+			}
+
+			host.Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+
+		//public static IHostBuilder CreateHostBuilder(string[] args) =>
+		//    Host.CreateDefaultBuilder(args)
+		//        .ConfigureWebHostDefaults(webBuilder =>
+		//        {
+		//            webBuilder.UseStartup<Startup>();
+		//        });
+	}
 }
--- src/TwelveWeekYear.GraphQL/Startup.cs
+++ src/TwelveWeekYear.GraphQL/Startup.cs
@@ -1,19 +1,14 @@
 using GraphQL.Server.Ui.Voyager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TwelveWeekYear.Infrastructure;
 
 namespace TwelveWeekYear.GraphQL
 {
-    public class Startup
+	public class Startup
     {
 		private readonly IConfiguration _configuration;
 
@@ -30,10 +25,10 @@ namespace TwelveWeekYear.GraphQL
 
 			services
 				.AddGraphQLServer()
-				.AddQueryType<Query>()
-				.AddFiltering()
-				.AddSorting()
-				.AddInMemorySubscriptions();
+				.AddQueryType<Query>();
+				//.AddFiltering()
+				//.AddSorting()
+				//.AddInMemorySubscriptions();
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
