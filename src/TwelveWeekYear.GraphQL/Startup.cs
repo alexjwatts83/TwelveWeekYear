929fd782f3df9e331340d3346e90da2ab293bd0a
Alex Watts <alexjwatts83@gmail.com>
2022-05-01 13:06:20 +1000

    wip query

--- src/TwelveWeekYear.GraphQL/Startup.cs
+++ src/TwelveWeekYear.GraphQL/Startup.cs
@@ -1,22 +1,40 @@
+using GraphQL.Server.Ui.Voyager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TwelveWeekYear.Infrastructure;
 
 namespace TwelveWeekYear.GraphQL
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+		private readonly IConfiguration _configuration;
+
+		public Startup(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
         {
-        }
+			services.AddInfrastructure(_configuration);
+
+			services
+				.AddGraphQLServer()
+				.AddQueryType<Query>()
+				.AddFiltering()
+				.AddSorting()
+				.AddInMemorySubscriptions();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,13 +46,15 @@ namespace TwelveWeekYear.GraphQL
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGraphQL();
+			});
+
+			app.UseGraphQLVoyager(new VoyagerOptions()
+			{
+				GraphQLEndPoint = "/graphql"
+			}, "/graphql-voyager");
+		}
     }
 }
