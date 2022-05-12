ab4b9dca63abb533ef02a71d4cb4cf4955d55608
Alex Watts <alexjwatts83@gmail.com>
2022-05-12 20:31:30 +1000

    Added mutation to update subtask

--- src/TwelveWeekYear.GraphQL/Mutations/Subtasks/SubtaskMutations.cs
+++ src/TwelveWeekYear.GraphQL/Mutations/Subtasks/SubtaskMutations.cs
@@ -1,6 +1,7 @@
 ﻿using HotChocolate;
 using HotChocolate.Types;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using TwelveWeekYear.Application.Interfaces;
@@ -12,9 +13,12 @@ namespace TwelveWeekYear.GraphQL.Mutations.Subtasks
 	public record AddSubtaskInput(string Description, int TaskId);
 	[GraphQLDescription("Add Subtask Payload.")]
 	public record AddSubtaskPayload(Subtask subtask);
+	[GraphQLDescription("Update Subtask Parameters.")]
+	public record UpdateSubtaskInput(string Description, int TaskId, int SubtaskId);
+	[GraphQLDescription("Update Subtask Payload.")]
+	public record UpdateSubtaskPayload(Subtask subtask);
 
 	[ExtendObjectType(OperationTypeNames.Mutation)]
-
 	public class SubtaskMutations : BaseGraphQLOperation
 	{
 		public SubtaskMutations(ILogger<BaseGraphQLOperation> logger, IAppDbContext dbContext)
@@ -36,6 +40,31 @@ namespace TwelveWeekYear.GraphQL.Mutations.Subtasks
 			await dbContext.SaveChangesAsync(cancellationToke);
 
 			return new AddSubtaskPayload(item);
+		}
+
+		[GraphQLDescription("Update Subtask.")]
+		public async Task<UpdateSubtaskPayload> UpdateTask(UpdateSubtaskInput input, CancellationToken cancellationToken)
+		{
+			var task = dbContext.Subtasks.FirstOrDefault(x => x.Id == input.SubtaskId);
+			if (task == null)
+			{
+				logger.LogWarning("subtask was null");
+				return new UpdateSubtaskPayload(task);
+			}
+			logger.LogInformation($"Input Description: {input.Description}");
+
+			task.Description = input.Description;
+			task.TaskId = input.TaskId;
+
+			var res = dbContext.Subtasks.Update(task);
+
+			task = res.Entity;
+
+			logger.LogInformation($"Updated Description: {task.Description}");
+
+			await dbContext.SaveChangesAsync(cancellationToken);
+
+			return new UpdateSubtaskPayload(task);
 		}
 	}
 }
