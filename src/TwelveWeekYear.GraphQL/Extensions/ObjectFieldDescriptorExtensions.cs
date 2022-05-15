45a5fbd86214411d9d83678b95727b0f286a42e5
Alex Watts <alexjwatts83@gmail.com>
2022-05-15 10:12:23 +1000

    using IObjectFieldDescriptor to hopefully resolve the AppDbContext issue

---
+++ src/TwelveWeekYear.GraphQL/Extensions/ObjectFieldDescriptorExtensions.cs
@@ -0,0 +1,34 @@
+﻿using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using TwelveWeekYear.Infrastructure.Persistence;
+
+namespace TwelveWeekYear.GraphQL.Extensions
+{
+	public static class ObjectFieldDescriptorExtensions
+	{
+		public static IObjectFieldDescriptor UseDbContext<TDbContext>(
+		 this IObjectFieldDescriptor descriptor
+		) where TDbContext : DbContext
+		{
+			return descriptor.UseScopedService<TDbContext>(
+				create: s => s.GetRequiredService<IDbContextFactory<TDbContext>>().CreateDbContext(),
+				disposeAsync: (s, c) => c.DisposeAsync()
+			);
+		}
+	}
+	public class UseMyWorldDbContextAttribute : ObjectFieldDescriptorAttribute
+	{
+		public override void OnConfigure(IDescriptorContext context,
+		IObjectFieldDescriptor descriptor, MemberInfo member)
+		{
+			descriptor.UseDbContext<AppDbContext>();
+		}
+	}
+}
