3f9389ddb567388bd1b00528a0459c023d41ba07
Alex Watts <alexjwatts83@gmail.com>
2022-05-17 07:33:52 +1000

    Added Custom DI file for GraphQL

---
+++ src/TwelveWeekYear.GraphQL/DependencyInjection/CustomServicesDependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TwelveWeekYear.Application.Interfaces;
+using TwelveWeekYear.GraphQL.Services;
+
+namespace TwelveWeekYear.GraphQL.DependencyInjection
+{
+	public static class CustomServicesDependencyInjection
+	{
+		public static IServiceCollection AddCustomServices(this IServiceCollection services)
+		{
+			services.AddHttpContextAccessor();
+			services.AddScoped<ICurrentUserService, CurrentUserService>();
+			return services;
+		}
+	}
+}
