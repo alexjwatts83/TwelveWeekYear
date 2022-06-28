84819d8bd1c18bf26309c613c58f51e5ce8be3a8
Alex Watts <alexjwatts83@gmail.com>
2022-06-29 07:37:25 +1000

    removed unused code

--- src/TwelveWeekYear.GraphQL/Startup.cs
+++ src/TwelveWeekYear.GraphQL/Startup.cs
@@ -26,7 +26,7 @@ namespace TwelveWeekYear.GraphQL
 
 			services.AddCustomServices();
 
-			services.AddCustomGraphQLServer(_configuration);
+			//services.AddCustomGraphQLServer(_configuration);
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
